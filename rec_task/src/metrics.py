a9efc53d30030fba6e426dccdecd771f1ff46491
hakubishin3 <hansaku187@gmail.com>
2021-05-05 09:52:22 +0000

    Add metric

---
+++ rec_task/src/metrics.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+from catalyst.metrics.functional import reciprocal_rank
+from catalyst.metrics.functional._misc import process_recsys_components
+
+
+def evaluate_rec_task_metrics(
+    output: torch.Tensor,
+    target: torch.Tensor,
+    top_k: int = 20,
+) -> dict:
+    # f1_score
+    output_top_k = output.argsort(descending=True)[:, :top_k]
+    all_f1 = []
+    for p, l in zip(output_top_k, target):
+        p = p.cpu().numpy()
+        l = l.cpu().numpy()
+        nb_hits = len(set(p).intersection(set(l)))
+        precision = nb_hits / top_k
+        recall = nb_hits / len(set(l)) if len(l) > 0 else 0.0
+        f1 = (2 * precision * recall) / (precision + recall) if (precision + recall) > 0 else 0.0
+        all_f1.append(f1)
+    f1_score = torch.Tensor(all_f1).mean()
+
+    # mrr
+    target_one_hot = nn.functional.one_hot(target.squeeze_(), num_classes=output.size()[1])
+    reciprocal_ranks = reciprocal_rank(output, target_one_hot, k=top_k)
+    mrr = reciprocal_ranks.mean()
+    
+    metrics = {
+        "f1_score": f1_score,
+        "mrr": mrr,
+    }
+    return metrics
--- rec_task/src/plmodel.py
+++ rec_task/src/plmodel.py
@@ -1,7 +1,10 @@
 import torch
 import torch.nn as nn
 import pytorch_lightning as pl
+from typing import List
+
 from src.models.transformer_encoder import TransformerEncoderModel
+from src.metrics import evaluate_rec_task_metrics
 
 
 class RecTaskPLModel(pl.LightningModule):
@@ -22,13 +25,34 @@ class RecTaskPLModel(pl.LightningModule):
         x_batch, y_batch = batch
         y_pred = self.forward(x_batch)
         loss = self.criterion(y_pred, y_batch.squeeze_())
-        return loss
+
+        self.log("loss", loss, prog_bar=True)
+        return {"loss": loss}
 
     def validation_step(self, batch, batch_idx):
         x_batch, y_batch = batch
         y_pred = self.forward(x_batch)
-        metric = 0
-        return metric
+        metrics = evaluate_rec_task_metrics(y_pred, y_batch)
+        loss = self.criterion(y_pred, y_batch.squeeze_())
+        metrics["loss"] = loss
+
+        self.log("val_loss", loss)
+        self.log_dict(metrics)
+        return metrics
+
+    def training_epoch_end(self, train_step_outputs: List[dict]):
+        loss = torch.stack([o["loss"] for o in train_step_outputs]).mean()
+
+        self.log("loss", loss)
+
+    def validation_epoch_end(self, val_step_outputs: List[dict]):
+        val_loss = torch.stack([o["loss"] for o in val_step_outputs]).mean()
+        val_f1_score = torch.stack([o["f1_score"] for o in val_step_outputs]).mean()
+        val_mrr = torch.stack([o["mrr"] for o in val_step_outputs]).mean()
+
+        self.log("val_loss", val_loss)
+        self.log("val_f1_score", val_f1_score)
+        self.log("val_mrr", val_mrr)
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(
