d4f21c3103571d4c5082c49f00642187d9576b97
hakubishin3 <hansaku187@gmail.com>
2021-05-05 11:41:44 +0000

    Add callbacks

--- rec_task/src/trainer.py
+++ rec_task/src/trainer.py
@@ -2,8 +2,21 @@ import pytorch_lightning as pl
 
 
 def get_trainer(config: dict) -> pl.Trainer:
+    ckpt_callback = pl.callbacks.ModelCheckpoint(
+        filename='{epoch:03d}-{val_mrr:.3f}-{val_f1_score:.3f}',
+        save_top_k=1,
+        monitor="val_mrr",
+        mode="max",
+    )
+    early_stop_callback = pl.callbacks.EarlyStopping(
+        monitor="val_mrr",
+        min_delta=0.00,
+        patience=3,
+        verbose=True,
+        mode="max",
+    )
     return pl.Trainer(
         max_epochs=config["n_epochs"],
-        profiler="simple",
+        callbacks=[ckpt_callback, early_stop_callback],
         gpus=1,
     )
