c2e55d08dd6600c14a32e547fc1f34a45c7fc0a3
hakubishin3 <hansaku187@gmail.com>
2021-05-06 14:33:12 +0000

    return sigmoid

--- rec_task/src/models/transformer_encoder.py
+++ rec_task/src/models/transformer_encoder.py
@@ -92,7 +92,7 @@ class TransformerEncoderModel(nn.Module):
         encoder_outputs = self.dropout(encoder_outputs)
         pooling =self.global_max_pooling_1d(encoder_outputs)
         logits = self.ffn(pooling)
-        return logits
+        return torch.sigmoid(logits)
 
 
 class GlobalMaxPooling1D(nn.Module):
