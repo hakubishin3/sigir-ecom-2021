e5dc94d7f38743ff96ce275807449ac35b950368
hakubishin3 <hansaku187@gmail.com>
2021-05-08 15:33:07 +0900

    Add debug_mode of trainer

--- rec_task/src/trainer.py
+++ rec_task/src/trainer.py
@@ -3,23 +3,27 @@ from pathlib import Path
 from pytorch_lightning.loggers import WandbLogger
 
 
-def get_trainer(config: dict, wandb_logger: WandbLogger = None) -> pl.Trainer:
+def get_trainer(
+    config: dict,
+    wandb_logger: WandbLogger = None,
+    debug: bool = False
+) -> pl.Trainer:
     ckpt_callback = pl.callbacks.ModelCheckpoint(
         dirpath=Path(config["file_path"]["output_dir"]) / config["exp_name"],
         filename='{epoch:03d}-{val_loss:.3f}-{val_mrr:.3f}-{val_f1_score:.3f}',
         save_top_k=1,
-        monitor="val_mrr",
-        mode="max",
+        monitor="val_loss",
+        mode="min",
     )
     early_stop_callback = pl.callbacks.EarlyStopping(
-        monitor="val_mrr",
+        monitor="val_loss",
         min_delta=0.00,
         patience=3,
         verbose=True,
-        mode="max",
+        mode="min",
     )
     return pl.Trainer(
-        max_epochs=config["n_epochs"],
+        max_epochs=config["n_epochs"] if not debug else 2,
         callbacks=[ckpt_callback, early_stop_callback],
         logger=wandb_logger,
         gpus=1,
